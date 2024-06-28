834cb557f72d728b807805de4a4e01d58131636a
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2024-06-28 11:15:07 +0000

    Add users and fix css in app

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
   selector: 'app-user',
@@ -8,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  public selectedUser = DUMMY_USERS[this.getRandomeUser()]
 
+  private getRandomeUser(): number {
+    return Math.floor(Math.random() * DUMMY_USERS.length);
+  }
 }
