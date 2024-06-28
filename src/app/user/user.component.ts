7cb47188cf8e5130ac4f14a6b58c545f8f345ab2
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2024-06-28 02:41:01 +0000

    Fix typo change users to user

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,9 +4,9 @@ import { Component } from '@angular/core';
   selector: 'app-users',
   standalone: true,
   imports: [],
-  templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
 })
-export class UsersComponent {
+export class UserComponent {
 
 }
