9829100ee6754d254b4c22e3efa34f1f11d6897f
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2024-07-01 20:56:09 +0000

    Add dummy task and interface

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,15 +1,25 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { dummyTasks } from '../dummy-tasks';
 
+interface Task {
+  id: string,
+  userId: string
+  title: string,
+  summary: string,
+  dueDate: string
+ }
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input()
   public name?: string;
+  public tasks: Task[] = dummyTasks;
 
 }
