ce6a4a007359bcc5a1e75c45b135f6c86f0eb6c2
Stephanie Viveros <50332616+MidKnight92@users.noreply.github.com>
2024-07-02 13:34:49 +0000

    Show add task on click

--- src/app/tasks/add-task/add-task.component.ts
+++ src/app/tasks/add-task/add-task.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { TasksComponent } from '../tasks.component';
+import { Task } from '../task/task.model';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [TasksComponent],
   templateUrl: './add-task.component.html',
   styleUrl: './add-task.component.css'
 })
-export class AddTaskComponent {
+  export class AddTaskComponent {
+  @Input()
+  public task?: Task;
 
+  public addNewTask(task: Task): void{
+    this.task = task;
+  }
 }
+
+
+/*
+   const newTaskValue = {
+      id: 't4',
+      userId: 'userId4',
+      title: 'Add new task',
+      summary: 'Add new component and render to screen.',
+      dueDate: '2024-07-02'
+     }
+*/
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -2,13 +2,14 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from '../dummy-tasks';
 import { Task } from './task/task.model';
+import { AddTaskComponent } from './add-task/add-task.component';
 
 
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [TaskComponent],
+  imports: [TaskComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
@@ -19,8 +20,7 @@ export class TasksComponent {
   @Input()
   public selectedUserId?: string;
 
-  @Output()
-  public newTask = new EventEmitter<string>();
+  public isAddNewTask = false;
 
   public tasks: Task[] = dummyTasks;
 
@@ -33,7 +33,7 @@ export class TasksComponent {
   }
 
   public addTask(): void {
-    this.newTask.emit('new task');
+    this.isAddNewTask = true;
   }
-
 }
+
